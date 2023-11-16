108e743e3015c854a095e3741ce9a5209e223369
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-11-16 16:21:15 +0100

    Update signin.jsx

--- src/pages/auth/signin.jsx
+++ src/pages/auth/signin.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 // import Footer from "../../Components/footer";
 import LoginSide from "../../components/loginside";
-import LoginHeader from "../../components/loginHeader";
-import LoginForm from '../../components/loginForm';
+// import LoginHeader from "../../components/loginHeader";
+// import LoginForm from '../../components/loginForm';
 
 
 const MentorLogin = () => {
   return (
     <>
       <div className="overflow-hidden">
-        <LoginHeader />
+        {/* <LoginHeader /> */}
         <div className="flex flex-col md:flex md:flex-row items-center justify-evenly">
           <LoginSide />
-          <LoginForm />
+          {/* <LoginForm /> */}
         </div>
       </div>
       <div className="-mt-20">
