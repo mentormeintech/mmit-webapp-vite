ec72f5bfa5aac02fd708f327b1cc9bd014062752
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2023-11-16 16:40:43 +0100

    Update NewsletterSection.jsx

--- src/features/landingPage/NewsletterSection.jsx
+++ src/features/landingPage/NewsletterSection.jsx
@@ -3,7 +3,7 @@ import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
 import Alert from '../Alert'
 import { postRequest } from "../../utilities/apiClient";
-import Loader from "../../Components/loader";
+import Loader from "../../components/loader";
 
 export const NewsletterSection = () => {
   const [loading, setloading] = useState(false)
