01de126decbabd690a9a8ae2873c56baed54be29
Segun Ajibola <ajbl.segun@gmail.com>
2024-01-23 22:55:33 +0100

    re-fix the newsletter design for desktop version

--- src/features/landingPage/NewsletterSection.jsx
+++ src/features/landingPage/NewsletterSection.jsx
@@ -48,14 +48,14 @@ export const NewsletterSection = () => {
           {/* <h2 className="text-2xl font-semibold text-secondary-500 sm:text-3xl">
             Newsletter
           </h2> */}
-          <h2 className="text-sm sm:text-base md:text-lg md:mt-3 text-center">
+          <h2 className="text-sm sm:text-base md:text-2xl md:mt-3 text-center">
             To get more information subscribe to our news letter
           </h2>
           <div className="flex max-w-3xl flex-col gap-4 sm:flex-row md:max-w-[90%] md:justify-center md:items-center md:mx-auto md:w-full">
-            <p className="w-[20%] md:font-semibold text-[#0F88D9]">
+            <p className="md:w-[35%] md:font-bold md:text-xl text-[#0F88D9]">
               Subscribe to our Newsletter
             </p>
-            <div className="flex flex-col md:flex-row bg-secondary-200 md:p-4 w-[60%]">
+            <div className="flex flex-col md:flex-row bg-secondary-200 md:p-4 md:w-[65%]">
               <input
                 type="email"
                 name="email"
@@ -69,7 +69,7 @@ export const NewsletterSection = () => {
                 disabled={loading === true ? true : false}
                 className={`${
                   loading === true ? "cursor-not-allowed" : "cursor-pointer"
-                } w-fit rounded border border-primary-500 bg-primary-500 px-6 py-2 text-sm font-medium text-white transition-all hover:bg-opacity-70`}
+                } w-fit rounded border border-[#0F88D9] bg-[#0F88D9] px-6 py-2 md:py-3 md:text-lg text-sm font-medium text-white transition-all hover:bg-opacity-70`}
               >
                 {loading ? <Loader /> : "Subscribe"}
               </button>
