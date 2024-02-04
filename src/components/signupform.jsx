ace1426c11d3dee0f6cdf2c02c3ac6ab29bcef26
mruqqie <mruqqie@gmail.com>
2024-02-04 05:56:49 +0100

    modified:   src/components/signupform.jsx 	modified:   src/components/signupheader.jsx 	modified:   src/pages/auth/mentorsignup.jsx

--- src/components/signupform.jsx
+++ src/components/signupform.jsx
@@ -65,7 +65,7 @@ const SignupForm = (props) => {
 
   // #0F88D
   return (
-    <div data-aos='fade-left' className="flex smd:w-[500px] flex-col justify-center pb-10 smd:relative smd:right-0 sm:flex sm:flex-col sm:justify-center lg:relative lg:right-8 xl:mx-auto">
+    <div data-aos='fade-left' className="flex w-[99%] smd:w-[460px] lg:w-[500px] border-black sm:w-[420px] md:w-[400px] xl:w-[500px] mt-8 sm:mt-0 lg:ml-0 md:ml-[-100px] flex-col justify-center pb-10 sm:pl-8 md:pl-0 smd:relative smd:right-0 sm:flex sm:flex-col sm:justify-center lg:relative lg:right-8 xl:mx-auto">
       <div data-aos='fade-left' className="text-base font-normal xl:w-full leading-relaxed text-neutral-700 smd:w-[90%] ">
         {`"Take the Leap and Join Our Transformative Mentorship Program,
       Where Passionate Individuals Connect, Learn, and Inspire Each
@@ -73,7 +73,7 @@ const SignupForm = (props) => {
       </div>
       <form
         data-aos='fade-left'
-        className="mt-5 flex w-9/12 md:w-[600px] flex-col justify-center text-base smd:ml-0"
+        className="mt-5  border-black flex smd:w-[133%] sm:w-[467px] md:w-[528px] xl:w-[600px] lg:w-[580px] flex-col justify-center text-base smd:ml-0"
         onSubmit={handleSubmit(registerUser)}
       >
         {/* <form
@@ -85,7 +85,7 @@ const SignupForm = (props) => {
           {/* <div className="flex flex-col smd:w-9/12"> */}
           <p className="xs:text-xl">First Name</p>
           <input
-            className="inline-flex h-12 w-28 items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 pl-5 pt-1.5 outline-none smd:w-full"
+            className="inline-flex h-12 w-[134%] items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 pl-5 pt-1.5 outline-none smd:w-full"
             type="text"
             placeholder="Ololade"
             {...register("first_name", { required: true })}
@@ -99,7 +99,7 @@ const SignupForm = (props) => {
         <div className="flex flex-col mt-3 mb-2 w-9/12">
           <p className="xs:text-xl ml-5 smd:ml-0">Last Name</p>
           <input
-            className="inline-flex h-12 w-44 items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 ml-5 smd:ml-0 pl-5 pt-1.5 outline-none smd:w-full"
+            className="inline-flex h-12 w-[134%] items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 ml-0 pl-5 pt-1.5 outline-none smd:w-full"
             type="text"
             placeholder="Martha"
             {...register("last_name", { required: true })}
@@ -113,7 +113,7 @@ const SignupForm = (props) => {
         <div className="flex flex-col mt-2 mb-2 w-9/12">
           <p className="xs:text-xl">Email Address</p>
           <input
-            className="inline-flex h-12 w-44 items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 ml-5 smd:ml-0 pl-5 pt-1.5 outline-none smd:w-full"
+            className="inline-flex h-12 w-[134%] items-center justify-start rounded-lg border border-black border-opacity-20 pb-2\ ml-0 pl-5 pt-1.5 outline-none smd:w-full"
             type="email"
             name="email"
             placeholder="123456789@gmail.com"
@@ -128,7 +128,7 @@ const SignupForm = (props) => {
         <div className="flex flex-col w-9/12">
           <p className="mt-3 xs:text-xl">Password</p>
           <input
-            className="inline-flex h-12 w-44 items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 ml-5 smd:ml-0 pl-5 pt-1.5 outline-none smd:w-full"
+            className="inline-flex h-12 w-[134%] items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 ml-0 pl-5 pt-1.5 outline-none smd:w-full"
             type="password"
             name="password"
             placeholder="********"
@@ -143,7 +143,7 @@ const SignupForm = (props) => {
         <div className="flex flex-col w-9/12">
           <p className="mt-3 xs:text-xl">Confirm Password</p>
           <input
-            className="inline-flex h-12 w-44 items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 ml-5 smd:ml-0 pl-5 pt-1.5 outline-none smd:w-full"
+            className="inline-flex h-12 w-[134%] items-center justify-start rounded-lg border border-black border-opacity-20 pb-2 ml-0 pl-5 pt-1.5 outline-none smd:w-full"
             type="password"
             name="repeat_password"
             placeholder="********"
@@ -155,10 +155,10 @@ const SignupForm = (props) => {
             </span>
           )}
         </div>
-        <div className="mt-8 w-[320px]">
+        <div className="mt-8 smd:w-[75%]">
           <div className="flex flex-col w-9/12">
             <button
-              className={`inline-flex h-14 items-center justify-center whitespace-nowrap rounded-2xl bg-sky-600 py-3.5 text-xl font-bold text-white smd:px-52 ${loading === true ? "cursor-not-allowed" : "cursor-pointer"
+              className={`inline-flex h-14 items-center justify-center whitespace-nowrap rounded-2xl bg-sky-600 py-3.5 text-xl font-bold text-white smd:px-[230px] sm:px-[175px] md:px-[200px] lg:px-[217px] xl:px-[225px] ${loading === true ? "cursor-not-allowed" : "cursor-pointer"
                 }`}
               disabled={loading === true ? true : false}
             >
@@ -173,7 +173,7 @@ const SignupForm = (props) => {
               </span>
             )}
           </div>
-          <div className="mt-1 flex items-center justify-center space-x-2">
+          <div className="mt-1 w-[100%] flex items-center smd:justify-center space-x-2">
             <div className="py-1 text-sm font-medium text-neutral-400">
               Already have an account?
             </div>
