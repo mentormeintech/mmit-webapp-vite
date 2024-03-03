3f4a7cdaa5bbf862096d45dbda541c5c321ae139
Segun Ajibola <ajbl.segun@gmail.com>
2024-03-03 19:21:04 +0100

    add support page

--- src/pages/menteesupport/page.jsx
+++ src/pages/menteesupport/page.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header";
+import MenteeSide from "../../components/MenteeSide";
 
 const MenteeSupport = () => {
   return (
-    <div>MenteeSupport page</div>
-    // this is the page you're looking for.
-  )
-}
+    <div className="pt-20 mx-3">
+      <Header />
+      <div className="flex">
+        <MenteeSide />
+        <section className="w-9/12 py-8 px-5">
+          <div className="flex gap-x-3">
+            <div className="border p-6">Help Center</div>
+            <div className="border p-6">Contact us</div>
+            <div className="border p-6">Privacy Policy</div>
+            <div className="border p-6">FAQ</div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
-export default MenteeSupport
+export default MenteeSupport;
