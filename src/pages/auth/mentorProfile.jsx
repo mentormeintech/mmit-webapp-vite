43a4829b94bd9ba86d9f934ad4974c0557f59e77
mruqqie <mruqqie@gmail.com>
2024-03-10 13:25:20 +0100

    modified:   src/pages/auth/mentorProfile.jsx

--- src/pages/auth/mentorProfile.jsx
+++ src/pages/auth/mentorProfile.jsx
@@ -21,7 +21,7 @@ const MentorProfile = () => {
 	};
 
 	return (
-		<div className="w-[98.9vw] overflow-x-hidden">
+		<div className="w-[100vw] overflow-x-hidden">
 			<Header />
 			<div
 				data-aos="fade-down"
