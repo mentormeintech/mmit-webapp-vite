3495b2df1025cfd5b22dbfd723a2b58fd332b98a
mruqqie <mruqqie@gmail.com>
2024-03-10 12:33:53 +0100

    new file:   src/assets/backarrow.png 	modified:   src/components/Header.jsx 	new file:   src/components/MentorCalendarCard.jsx 	new file:   src/components/MentorCard.jsx 	modified:   src/pages/auth/mentorProfile.jsx 	modified:   src/pages/bookasession.jsx

---
+++ src/components/MentorCalendarCard.jsx
@@ -0,0 +1,193 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import { Modal, Box, Radio, RadioGroup, FormControlLabel } from "@mui/material";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+function MentorCalendarCard() {
+	const [view, setView] = useState("month");
+	const [events, setEvents] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [selectedDate, setSelectedDate] = useState(null);
+	const [selectedTime, setSelectedTime] = useState(null);
+
+	useEffect(() => {
+		const sampleEvents = [
+			{
+				title: "Event 1",
+				start: new Date("2024-03-10T09:00:00"),
+				end: new Date("2024-03-10T10:00:00"),
+			},
+			{
+				title: "Event 2",
+				start: new Date("2024-03-12T11:00:00"),
+				end: new Date("2024-03-12T12:00:00"),
+			},
+		];
+		setEvents(sampleEvents);
+	}, []);
+
+	const handleSelectSlot = ({ start }) => {
+		setSelectedDate(start);
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+		setSelectedDate(null);
+	};
+
+	const handleAddEvent = () => {
+		const currentDate = new Date();
+		currentDate.setDate(currentDate.getDate() - 1);
+
+		if (selectedDate < currentDate) {
+			alert("You cannot add events on past dates.");
+			return;
+		}
+		if (selectedDate && selectedTime) {
+			const [startTime, endTime] = selectedTime.split("-");
+			const [startHour, startMinute] = startTime.split(":");
+			const [endHour, endMinute] = endTime.split(":");
+			const startDate = new Date(selectedDate);
+			startDate.setHours(parseInt(startHour), parseInt(startMinute));
+			const endDate = new Date(selectedDate);
+			endDate.setHours(parseInt(endHour), parseInt(endMinute));
+			const newEvent = {
+				title: "Booked	",
+				start: startDate,
+				end: endDate,
+			};
+			setEvents([...events, newEvent]);
+			handleClose();
+		}
+	};
+
+	const CustomEvent = ({ event }) => {
+		const height =
+			event.end - event.start > 3600000
+				? `${Math.max(20, (event.end - event.start) / 1800000)}px`
+				: "auto";
+		const monthViewStyle = {
+			backgroundColor: "#7393B3",
+			color: "white",
+			padding: "4px",
+			borderRadius: "3px",
+			cursor: "pointer",
+			width: "100%",
+			height: height,
+		};
+		const weekViewStyle = {
+			backgroundColor: "#7393B3",
+			color: "white",
+			padding: "4px",
+			marginLeft: "-128px",
+			borderRadius: "3px",
+			cursor: "pointer",
+			width: "100%",
+			height: height,
+		};
+
+		const getViewStyle = () => {
+			switch (view) {
+				case "month":
+					return monthViewStyle;
+				case "week":
+					return weekViewStyle;
+				case "day":
+					return weekViewStyle;
+				default:
+					return monthViewStyle;
+			}
+		};
+
+		return <div style={getViewStyle()}>{event.title}</div>;
+	};
+
+	return (
+		<div className="relative h-screen">
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+				selectable
+				onSelectSlot={handleSelectSlot}
+				components={{
+					event: CustomEvent,
+				}}
+				onView={(newView) => setView(newView)} // Update the view state when the view changes
+				style={{ padding: 0, margin: 0 }}
+			/>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-title"
+				aria-describedby="modal-description"
+			>
+				<Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded">
+					<RadioGroup
+						value={selectedTime}
+						onChange={(e) => setSelectedTime(e.target.value)}
+					>
+						<FormControlLabel
+							value="8:00am-9:00am"
+							control={<Radio />}
+							label="8:00am - 9:00am"
+						/>
+						<FormControlLabel
+							value="9:00am-10:00am"
+							control={<Radio />}
+							label="9:00am - 10:00am"
+						/>{" "}
+						<FormControlLabel
+							value="10:00am-11:00am"
+							control={<Radio />}
+							label="10:00am - 11:00am"
+						/>
+						<FormControlLabel
+							value="11:00am-12:00pm"
+							control={<Radio />}
+							label="11:00am - 12:00pm"
+						/>
+						<FormControlLabel
+							value="12:00pm-1:00pm"
+							control={<Radio />}
+							label="12:00pm - 1:00pm"
+						/>
+						<FormControlLabel
+							value="1:00pm-2:00pm"
+							control={<Radio />}
+							label="1:00pm - 2:00pm"
+						/>
+						<FormControlLabel
+							value="2:00pm-3:00pm"
+							control={<Radio />}
+							label="2:00pm - 3:00pm"
+						/>
+						<FormControlLabel
+							value="3:00pm-4:00pm"
+							control={<Radio />}
+							label="3:00pm - 4:00pm"
+						/>
+						<FormControlLabel
+							value="4:00pm-5:00pm"
+							control={<Radio />}
+							label="4:00pm - 5:00pm"
+						/>
+					</RadioGroup>
+					<button
+						onClick={handleAddEvent}
+						className="bg-blue-500 text-white px-4 py-2 rounded"
+					>
+						Add Event
+					</button>
+				</Box>
+			</Modal>
+		</div>
+	);
+}
+
+export default MentorCalendarCard;
--- src/pages/auth/mentorProfile.jsx
+++ src/pages/auth/mentorProfile.jsx
@@ -4,140 +4,147 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { GrMail } from "react-icons/gr";
 import { useParams } from "react-router-dom";
+import MentorCard from "../../components/MentorCard";
+import MentorCalendarCard from "../../components/MentorCalendarCard";
+import Backarrow from "../../assets/backarrow.png";
+import { useState } from "react";
 
 const MentorProfile = () => {
+	const { mentor_name } = useParams();
+	const [card, setCard] = useState(true);
+	// console.log("mentor_name", mentor_name);
+	const showProfileCard = () => {
+		setCard(true);
+	};
+	const showCalendarCard = () => {
+		setCard(false);
+	};
 
-  const { mentor_name } = useParams()
-  console.log('mentor_name', mentor_name)
-
-  return (
-    <div>
-      <Header />
-      <div data-aos="fade-down" className="background-dp relative w-[100%]">
-        <img
-          data-aos="fade-down"
-          src="/images/background.png"
-          width={2000}
-          alt="mentor-dp"
-          className="absolute w-[100%]"
-          height={50}
-        />
-      </div>
-      <div className="relative" data-aos="fade-right">
-        <img
-          className="relative left-24 top-36 h-[200px] w-[200px] rounded-full"
-          src="/images/profileImage1.png"
-          width={2000}
-          alt="profilephoto"
-          height={50}
-        />
-        <div
-          data-aos="fade-right"
-          className="bg-grey relative left-80 top-11 mt-2
+	return (
+		<div className="w-[98.9vw] overflow-x-hidden">
+			<Header />
+			<div
+				data-aos="fade-down"
+				className="background-dp relative w-[100%]"
+			>
+				<img
+					data-aos="fade-down"
+					src="/images/background.png"
+					width={2000}
+					alt="mentor-dp"
+					className="absolute w-[100%]"
+					height={50}
+				/>
+			</div>
+			<div className="relative" data-aos="fade-right">
+				<img
+					className="relative left-24 top-36 h-[200px] w-[200px] rounded-full"
+					src="/images/profileImage1.png"
+					width={2000}
+					alt="profilephoto"
+					height={50}
+				/>
+				<div
+					data-aos="fade-right"
+					className="bg-grey relative left-80 top-11 mt-2
         "
-        >
-          <h1 className="text-4xl font-semibold capitalize mb-2">{mentor_name || 'NIL'}</h1>
-          <p className="texl-xl">Product Designer at PDI ltd.</p>
-        </div>
-      </div>
-      <div data-aos="fade-up-right" data-aos-easing="linear"
-        data-aos-duration="900" className="m-auto mt-44 max-w-screen-xl px-11">
-        <h1 className="mb-5 text-4xl font-semibold">Overview</h1>
-        <div className="flex max-w-screen-xl flex-col items-center rounded-md border bg-[#E4CBC81A] p-8 text-left text-base">
-          <div className="h-auto">
-            <span className="text-base font-medium leading-7 text-zinc-800">
-              Hi, I am Amarachi, a Product Designer with nearly four years of
-              experience, my background primarily lies in Visual Design.
-              <br />
-              <br />
-              For the past years, I dedicated part of my role to fostering a
-              strong design team culture. I believe in the power of scaling
-              design rituals and maintaining a high-quality bar as the team
-              expands. I believe in the value of collaborative problem-solving
-              to drive innovative solutions and in transparent and honest
-              communication to foster alignment and drive outcomes. I wish I
-              could tell you I learned this the easy way.
-              <br />
-              <br />I believe I have a lot to share with others when it comes to
-              <br />
-              <br />
-            </span>
-            <span className="text-base font-medium leading-7 text-zinc-800">
-              - dealing with complexity and ambiguity in this new world of
-              uncertainty
-              <br />
-              - how to articulate design decisions <br />
-              - ways of working
-              <br />
-              - design career path
-              <br />
-              - build your portfolio case or improve existing
-              <br />
-              - design career path
-              <br />
-              - figma, design systems, UI design
-              <br />
-              - UX research methods
-              <br />
-              - design process
-              <br />
-            </span>
-            <span className="text-base font-medium leading-7 text-zinc-800"></span>
-          </div>
-        </div>
-        <div data-aos="zoom-in" data-aos-offset="300"
-          data-aos-easing="ease-in-sine" className="mt-8 inline-flex h-10 w-44 items-center justify-start gap-5">
-          <div className="h-8 w-8">
-            <div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
-              <FaTwitter />
-            </div>
-          </div>
-          <div className="h-8 w-8">
-            <div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
-              <GrMail />
-            </div>
-          </div>
-          <div className="h-8 w-8">
-            <div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
-              <FaLinkedinIn />
-            </div>
-          </div>
-        </div>
-        <div ata-aos="zoom-in" data-aos-offset="300"
-          data-aos-easing="ease-in-sine" className="mt-11 flex justify-between">
-          <div ata-aos="zoom-in" data-aos-offset="300"
-          data-aos-easing="ease-in-sine">
-            <h1 className="whitespace-nowrap text-4xl font-semibold text-black">
-              Community Statistics
-            </h1>
-            <p className="text-xl font-medium text-neutral-700">
-              Top areas of impact
-            </p>
-            <p className="text-base font-normal text-neutral-700">
-              Topics to be discussed during session
-            </p>
-          </div>
-          <div className="topics flex  flex-row items-end gap-6 whitespace-nowrap text-sm">
-            <div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-              General mentorship
-            </div>
-            <div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-              Design career path
-            </div>
-            <div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-              UX meaning
-            </div>
-            <div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-              Interaction design
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="mt-20">
-        <Footer />
-      </div>
-    </div>
-  );
+				>
+					<h1 className="text-4xl font-semibold capitalize mb-2">
+						{mentor_name || "NIL"}
+					</h1>
+					<p className="texl-xl">Product Designer at PDI ltd.</p>
+				</div>
+			</div>
+
+			<div
+				data-aos="fade-up-right"
+				data-aos-easing="linear"
+				data-aos-duration="900"
+				className="m-auto mt-44 max-w-screen-xl px-11"
+			>
+				<div className="flex justify-end">
+					{card ? (
+						<button
+							className="bg-primary-500 hover:bg-opacity-70 w-[200px] text-center py-2 text-white rounded-md font-semibold"
+							onClick={showCalendarCard}
+						>
+							Book a session
+						</button>
+					) : (
+						<img
+							src={Backarrow}
+							alt="Back Arrow"
+							className="w-5 h-5 cursor-pointer"
+							onClick={showProfileCard}
+						/>
+					)}
+				</div>
+				<h1 className="mb-5 text-4xl font-semibold">Overview</h1>
+				{card ? <MentorCard /> : <MentorCalendarCard />}
+				<div
+					data-aos="zoom-in"
+					data-aos-offset="300"
+					data-aos-easing="ease-in-sine"
+					className="mt-8 inline-flex h-10 w-44 items-center justify-start gap-5"
+				>
+					<div className="h-8 w-8">
+						<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
+							<FaTwitter />
+						</div>
+					</div>
+					<div className="h-8 w-8">
+						<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
+							<GrMail />
+						</div>
+					</div>
+					<div className="h-8 w-8">
+						<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
+							<FaLinkedinIn />
+						</div>
+					</div>
+				</div>
+				<div
+					ata-aos="zoom-in"
+					data-aos-offset="300"
+					data-aos-easing="ease-in-sine"
+					className="mt-11 flex justify-between"
+				>
+					<div
+						ata-aos="zoom-in"
+						data-aos-offset="300"
+						data-aos-easing="ease-in-sine"
+					>
+						<h1 className="whitespace-nowrap text-4xl font-semibold text-black">
+							Community Statistics
+						</h1>
+						<p className="text-xl font-medium text-neutral-700">
+							Top areas of impact
+						</p>
+						<p className="text-base font-normal text-neutral-700">
+							Topics to be discussed during session
+						</p>
+					</div>
+					<div className="topics flex  flex-row items-end gap-6 whitespace-nowrap text-sm">
+						<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+							General mentorship
+						</div>
+						<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+							Design career path
+						</div>
+						<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+							UX meaning
+						</div>
+						<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+							Interaction design
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="mt-20">
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default MentorProfile;
