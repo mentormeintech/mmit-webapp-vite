d9f0c02fac126f6b813257c39979d12f750bca1d
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2024-03-11 16:34:34 +0100

    Update apiClient.js

--- src/utilities/apiClient.js
+++ src/utilities/apiClient.js
@@ -136,39 +136,3 @@ export const logUserOut = async (url) => {
         return { status: error?.response?.status || 500, message: error?.response?.data?.message || error?.message, success: false };
     }
 };
-
-// export const createUserWithGoogleSignIn = async (formData) => {
-//     try {
-//         const response = await useAxios.post(`/user/social`, formData);
-//         const { data, status } = response;
-//         if (status === 200 && data.success === false) {
-//             return { data: {}, status, success: data.success, message: data?.message };
-//         } else if (status === 200 && data.success === true) {
-//             return { data: data.payload, status, success: data.success, message: data?.message };
-//         }
-//     } catch (error) {
-
-//         return { status: error?.response?.status || 500, message: error?.response?.data?.message || error?.message, success: false };
-//     }
-// };
-
-
-// export const signInUserUsingGoogle = async (formData) => {
-//     try {
-//         const response = await useAxios.post(`/user/social/google`, formData);
-//         const { data, status } = response;
-//         if (status === 200 && data.success === false) {
-//             return { data: {}, status, success: data.success, message: data?.message };
-//         } else if (status === 200 && data.success === true) {
-//             await storeData(accessToken, data.token)
-//             await AsyncStorage.setItem(`${accessToken}`, data.token)
-//             setToken(getValidToken())
-//             return { data: data.payload, status, success: data.success, message: data?.message, token: data.token };
-//         }
-//     } catch (error) {
-
-//         return { status: error?.response?.status || 500, message: error?.response?.data?.message || error?.message, success: false };
-//     }
-// };
-
-
