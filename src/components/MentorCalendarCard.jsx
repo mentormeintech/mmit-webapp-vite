339ab0c71daf485d1fee8190da8ea2427918273a
ExciteNG <65714464+dolaposokoya@users.noreply.github.com>
2024-03-11 16:34:04 +0100

    connected book a session with mentor

--- src/components/MentorCalendarCard.jsx
+++ src/components/MentorCalendarCard.jsx
@@ -1,9 +1,16 @@
-import { Calendar,Views, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import React, { useState, useEffect } from "react";
 import moment from "moment";
-import { Modal, Box, Radio, RadioGroup, FormControlLabel } from "@mui/material";
+import { Modal, Box } from "@mui/material";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { ScheduleView } from "../styled/component";
+import SessionTime from "./sessionTime";
+import Alert from "../features/Alert";
+import { getValidToken } from "../utilities/tokenClient";
+import { useNavigate } from "react-router-dom";
+import { postRequest } from "../utilities/apiClient";
+import { setToken } from "../utilities/axiosClient";
+import Spinner from "./Spinner";
 
 const localizer = momentLocalizer(moment);
 
@@ -12,9 +19,12 @@ function MentorCalendarCard(props) {
 	const [view, setView] = useState("month");
 	const [events, setEvents] = useState([]);
 	const [open, setOpen] = useState(false);
+	const [loading, setloading] = useState(false)
 	const [selectedDate, setSelectedDate] = useState(null);
 	const [selectedTime, setSelectedTime] = useState(null);
-	console.log('mentorEvents', mentorEvent)
+	const [currentEvent, setcurrentEvent] = useState(null)
+	const navigation = useNavigate()
+
 	useEffect(() => {
 		setEvents(mentorEvent);
 	}, []);
@@ -25,70 +35,93 @@ function MentorCalendarCard(props) {
 		setOpen(true);
 	};
 
+	const sendAmPm = (date) => {
+		let newDate = new Date(date).getHours();
+		if (newDate < 12) {
+			return `${newDate}AM`
+		}
+		else {
+			newDate = newDate % 12
+			return `${newDate}PM`
+		}
+	};
+
+
 	const handleClose = () => {
 		setOpen(false);
 		setSelectedDate(null);
 	};
 
-	const handleAddEvent = () => {
-		const currentDate = new Date();
-		currentDate.setDate(currentDate.getDate() - 1);
-
-		if (selectedDate < currentDate) {
-			alert("You cannot add events on past dates.");
-			return;
-		}
-		if (selectedDate && selectedTime) {
-			const [startTime, endTime] = selectedTime.split("-");
-			const [startHour, startMinute] = startTime.split(":");
-			const [endHour, endMinute] = endTime.split(":");
-			const startDate = new Date(selectedDate);
-			startDate.setHours(parseInt(startHour), parseInt(startMinute));
-			const endDate = new Date(selectedDate);
-			endDate.setHours(parseInt(endHour), parseInt(endMinute));
-			const newEvent = {
-				title: "Booked	",
-				start: startDate,
-				end: endDate,
-			};
-			setEvents([...events, newEvent]);
-			handleClose();
+	const handleAddEvent = async () => {
+		try {
+			if (!selectedTime) {
+				return Alert('Please pick a time', 'warning')
+			}
+			if (new Date(selectedTime).getHours() < new Date(selectedDate).getHours()) {
+				return Alert(`Please select time from ${sendAmPm(selectedDate)}`, 'warning')
+			}
+			if (!currentEvent) {
+				return Alert('Please pick a schedule', 'warning')
+			}
+			setOpen(false);
+			setloading(true)
+			const response = await postRequest(`event/book?time=${new Date(selectedTime)}&event_id=${currentEvent._id}`, {})
+			if (response && response.success === true) {
+				setOpen(false);
+				setloading(false)
+				return Alert(`Session booked! Please wait for mentor's response`, 'success')
+			}
+			else {
+				setOpen(false);
+				setloading(false)
+				return Alert(`${response.message}`, 'warning')
+			}
+			setloading(false)
+		} catch (error) {
+			setOpen(false);
+			setloading(false)
+			return Alert(`${error.message}`, 'warning')
 		}
 	};
 
 
-	const handleSelectEvent = (event) => {
+	const handleSelectEvent = async (event) => {
 		const { title, end, start } = event
-		alert(title)
+		setcurrentEvent(event)
+		const token = await getValidToken()
+		if (!token) {
+			return navigation('/auth/signin')
+		}
+		await setToken(token)
+		setSelectedDate(start);
+		return setOpen(true);
 	}
 
 	const components = {
-        event: (props) => {
-            const eventType = props?.event?.data?.type;
-            const bg = props?.event?.bg;
-            return (
-                <ScheduleView className='capitalize ScheduleView' bg={bg}>
-                    {props.title.substring(0, 19 - 3) + '...' || 'No Title'}
-                </ScheduleView>
-            )
-        },
+		event: (props) => {
+			const eventType = props?.event?.data?.type;
+			const bg = props?.event?.bg;
+			return (
+				<ScheduleView className='capitalize ScheduleView' bg={bg}>
+					{props.title.substring(0, 19 - 3) + '...' || 'No Title'}
+				</ScheduleView>
+			)
+		},
 	};
-	
+
 	return (
 		<div className="relative h-screen">
-			<Calendar
+			{loading ? <Spinner loading={loading} /> : <Calendar
 				localizer={localizer}
 				events={events}
 				startAccessor="start"
 				endAccessor="end"
 				selectable
-				// defaultView={Views.WEEK}
 				onSelectEvent={handleSelectEvent}
-				// onSelectSlot={handleSelectSlot}
 				components={components}
 				onView={(newView) => setView(newView)}
 				style={{ padding: 0, margin: 0 }}
-			/>
+			/>}
 			<Modal
 				open={open}
 				onClose={handleClose}
@@ -96,62 +129,7 @@ function MentorCalendarCard(props) {
 				aria-describedby="modal-description"
 			>
 				<Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded">
-					<RadioGroup
-						value={selectedTime}
-						onChange={(e) => setSelectedTime(e.target.value)}
-					>
-						<FormControlLabel
-							value="8:00am-9:00am"
-							control={<Radio />}
-							label="8:00am - 9:00am"
-						/>
-						<FormControlLabel
-							value="9:00am-10:00am"
-							control={<Radio />}
-							label="9:00am - 10:00am"
-						/>{" "}
-						<FormControlLabel
-							value="10:00am-11:00am"
-							control={<Radio />}
-							label="10:00am - 11:00am"
-						/>
-						<FormControlLabel
-							value="11:00am-12:00pm"
-							control={<Radio />}
-							label="11:00am - 12:00pm"
-						/>
-						<FormControlLabel
-							value="12:00pm-1:00pm"
-							control={<Radio />}
-							label="12:00pm - 1:00pm"
-						/>
-						<FormControlLabel
-							value="1:00pm-2:00pm"
-							control={<Radio />}
-							label="1:00pm - 2:00pm"
-						/>
-						<FormControlLabel
-							value="2:00pm-3:00pm"
-							control={<Radio />}
-							label="2:00pm - 3:00pm"
-						/>
-						<FormControlLabel
-							value="3:00pm-4:00pm"
-							control={<Radio />}
-							label="3:00pm - 4:00pm"
-						/>
-						<FormControlLabel
-							value="4:00pm-5:00pm"
-							control={<Radio />}
-							label="4:00pm - 5:00pm"
-						/>
-					</RadioGroup>
-					<button
-						onClick={handleAddEvent}
-						className="bg-blue-500 text-white px-4 py-2 rounded"
-					>
-						Add Event
-					</button>
+					<SessionTime handleAddEvent={handleAddEvent} setSelectedTime={setSelectedTime} selectedDate={selectedDate} />
 				</Box>
 			</Modal>
 		</div>
--- src/utilities/tokenClient.js
+++ src/utilities/tokenClient.js
@@ -1,11 +1,12 @@
-export const accessToken = 'MENTOR_ME_USER_ACCESS_TOKEN_' 
-export const mentorAccess = 'MENTOR_ME_USER_ID_' 
+export const accessToken = 'MENTOR_ME_USER_ACCESS_TOKEN_'
+export const mentorAccess = 'MENTOR_ME_USER_ID_'
 export async function getValidToken() {
     try {
         const token = sessionStorage.getItem(`${accessToken}`)
         if (token === null) {
             return null
         }
+        sessionStorage.setItem(accessToken, token)
         return token;
     } catch (error) {
         return null;
