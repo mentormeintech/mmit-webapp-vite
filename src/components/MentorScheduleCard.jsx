ca5cb7ccb0d960be29a00e77bab8485a5b6e9ecf
mruqqie <mruqqie@gmail.com>
2024-04-03 14:49:26 +0100

    deleted:    src/components/MentorCalendarCard.jsx 	new file:   src/components/MentorScheduleCard.jsx 	modified:   src/pages/auth/mentorProfile.jsx

---
+++ src/components/MentorScheduleCard.jsx
@@ -0,0 +1,67 @@
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import { Modal, Box } from "@mui/material";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { ScheduleView } from "../styled/component";
+import SessionTime from "./sessionTime";
+import Alert from "../features/Alert";
+import { getValidToken } from "../utilities/tokenClient";
+import { useNavigate } from "react-router-dom";
+import { postRequest } from "../utilities/apiClient";
+import { setToken } from "../utilities/axiosClient";
+import Spinner from "./Spinner";
+
+const localizer = momentLocalizer(moment);
+
+function MentorScheduleCard(props) {
+	const { mentorEvent } = props;
+	const [view, setView] = useState("month");
+	const [events, setEvents] = useState([]);
+	const [open, setOpen] = useState(false);
+	const [loading, setloading] = useState(false);
+	const [selectedDate, setSelectedDate] = useState(null);
+	const [selectedTime, setSelectedTime] = useState(null);
+	const [currentEvent, setcurrentEvent] = useState(null);
+	const navigation = useNavigate();
+
+	useEffect(() => {
+		setEvents(mentorEvent);
+	}, []);
+
+	return (
+		<div className="relative h-max pb-20">
+			{loading ? (
+				<Spinner loading={loading} />
+			) : (
+				<div className="flex flex-wrap items-center mx-auto justify-between gap-6 w-max max-w-[90%] border p-4 rounded-2xl shadow-lg">
+					{mentorEvent?.map((event) => (
+						<div className="rounded-md border shadow-md w-72 h-max">
+							<div
+								className="text-center text-xl rounded-t-md p-4 font-semibold"
+								style={{
+									backgroundColor: event.bg,
+								}}
+							>
+								Available
+							</div>
+							<div className="p-2 flex flex-col items-center">
+								<div className="text-3xl font-semibold mb-3">
+									{moment(event.start).format("h:mma")}
+								</div>
+								<div className="text-2xl font-semibold">
+									{moment(event.start).format("MMMM D")},
+								</div>
+								<div className="text-2xl font-semibold">
+									{moment(event.start).format("YYYY")}
+								</div>
+							</div>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
+}
+
+export default MentorScheduleCard;
--- src/pages/auth/mentorProfile.jsx
+++ src/pages/auth/mentorProfile.jsx
@@ -5,20 +5,21 @@ import { FaTwitter } from "react-icons/fa";
 import { GrMail } from "react-icons/gr";
 import { useNavigate, useParams } from "react-router-dom";
 import MentorCard from "../../components/MentorCard";
-import MentorCalendarCard from "../../components/MentorCalendarCard";
+import MentorScheduleCard from "../../components/MentorScheduleCard";
 import Backarrow from "../../assets/backarrow.png";
 import { useState, useLayoutEffect } from "react";
 import { mentorAccess } from "../../utilities/tokenClient";
 import { userGetRequest } from "../../utilities/apiClient";
 import Alert from "../../features/Alert";
 import Spinner from "../../components/Spinner";
+import { motion } from "framer-motion";
 
 const MentorProfile = () => {
 	const { mentor_name } = useParams();
-	const navigation = useNavigate()
+	const navigation = useNavigate();
 	const [loading, setloading] = useState(false);
 	const [card, setCard] = useState(true);
-	const [mentorEvents, setmentorEvents] = useState([])
+	const [mentorEvents, setmentorEvents] = useState([]);
 	const showProfileCard = () => {
 		setCard(true);
 	};
@@ -27,157 +28,170 @@ const MentorProfile = () => {
 	};
 
 	useLayoutEffect(() => {
-		getMentorEvent()
-	}, [])
+		getMentorEvent();
+	}, []);
 
 	async function getMentorEvent() {
 		try {
-			const mentor_id = localStorage.getItem(mentorAccess)
+			const mentor_id = localStorage.getItem(mentorAccess);
 			if (!mentor_id) {
-				return navigation('/findamentor')
+				return navigation("/findamentor");
 			}
-			setloading(true)
-			const response = await userGetRequest(`event/mentor/events?mentor_id=${mentor_id}`)
+			setloading(true);
+			const response = await userGetRequest(
+				`event/mentor/events?mentor_id=${mentor_id}`
+			);
 			if (response && response.success === true) {
-				setmentorEvents(response?.data)
-				setloading(false)
+				setmentorEvents(response?.data);
+				console.log(response.data);
+				setloading(false);
+			} else {
+				Alert(response.message, "warning");
+				setloading(false);
 			}
-			else {
-				Alert(response.message, 'warning')
-				setloading(false)
-			}
-			setloading(false)
+			setloading(false);
 		} catch (error) {
-			Alert(error.message, 'error')
-			setloading(false)
+			Alert(error.message, "error");
+			setloading(false);
 		}
 	}
 
 	return (
 		<>
-			{loading ? <Spinner /> : <div className="w-[100vw] overflow-x-hidden overflow-y-hidden">
-				<Header />
-				<div
-					data-aos="fade-down"
-					className="background-dp relative w-[100%]"
-				>
-					<img
-						data-aos="fade-down"
-						src="/images/background.png"
-						width={2000}
-						alt="mentor-dp"
-						className="absolute w-[100%]"
-						height={50}
-					/>
-				</div>
-				<div className="relative" data-aos="fade-right">
-					<img
-						className="relative left-24 top-36 h-[200px] w-[200px] rounded-full"
-						src="/images/profileImage1.png"
-						width={2000}
-						alt="profilephoto"
-						height={50}
-					/>
-					<div
-						data-aos="fade-right"
-						className="bg-grey relative left-80 top-11 mt-2
-        "
+			{loading ? (
+				<Spinner />
+			) : (
+				<div className="w-[100vw] overflow-x-hidden overflow-y-hidden">
+					<Header />
+					<motion.div
+						initial={{ y: -200 }}
+						animate={{ y: 0 }}
+						transition={{ type: "tween", stiffness: 120 }}
+						className="background-dp relative w-[100%]"
 					>
-						<h1 className="text-4xl font-semibold capitalize mb-2">
-							{mentor_name || "NIL"}
-						</h1>
-						<p className="texl-xl">Product Designer at PDI ltd.</p>
-					</div>
-				</div>
-
-				<div
-					data-aos="fade-up-right"
-					data-aos-easing="linear"
-					data-aos-duration="900"
-					className="m-auto mt-44 max-w-screen-xl px-11"
-				>
-					<div className="flex justify-end">
-						{card ? (
-							<button
-								className="bg-primary-500 hover:bg-opacity-70 w-[200px] text-center py-2 text-white rounded-md font-semibold"
-								onClick={showCalendarCard}
-							>
-								Book a session
-							</button>
-						) : (
-							<img
-								src={Backarrow}
-								alt="Back Arrow"
-								className="w-5 h-5 cursor-pointer"
-								onClick={showProfileCard}
-							/>
-						)}
-					</div>
-					<h1 className="mb-5 text-4xl font-semibold">{card ? 'Overview' : 'Session'}</h1>
-					{card ? <MentorCard /> : <MentorCalendarCard mentorEvent={mentorEvents} />}
-					<div
-						data-aos="zoom-in"
-						data-aos-offset="300"
-						data-aos-easing="ease-in-sine"
-						className="mt-8 inline-flex h-10 w-44 items-center justify-start gap-5"
+						<img
+							src="/images/background.png"
+							width={2000}
+							alt="mentor-dp"
+							className="absolute w-[100%]"
+							height={50}
+						/>
+					</motion.div>
+					<motion.div
+						className="relative"
+						initial={{ x: -300 }}
+						animate={{ x: 0 }}
+						transition={{ type: "spring", stiffness: 80 }}
 					>
-						<div className="h-8 w-8">
-							<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
-								<FaTwitter />
-							</div>
-						</div>
-						<div className="h-8 w-8">
-							<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
-								<GrMail />
-							</div>
-						</div>
-						<div className="h-8 w-8">
-							<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
-								<FaLinkedinIn />
-							</div>
-						</div>
-					</div>
-					<div
-						ata-aos="zoom-in"
-						data-aos-offset="300"
-						data-aos-easing="ease-in-sine"
-						className="mt-11 flex justify-between"
-					>
+						<img
+							className="relative left-24 top-36 h-[200px] w-[200px] rounded-full"
+							src="/images/profileImage1.png"
+							width={2000}
+							alt="profilephoto"
+							height={50}
+						/>
 						<div
-							ata-aos="zoom-in"
-							data-aos-offset="300"
-							data-aos-easing="ease-in-sine"
+							className="bg-grey relative left-80 top-11 mt-2"
 						>
-							<h1 className="whitespace-nowrap text-4xl font-semibold text-black">
-								Community Statistics
+							<h1 className="text-4xl font-semibold capitalize mb-2">
+								{mentor_name || "NIL"}
 							</h1>
-							<p className="text-xl font-medium text-neutral-700">
-								Top areas of impact
-							</p>
-							<p className="text-base font-normal text-neutral-700">
-								Topics to be discussed during session
+							<p className="texl-xl">
+								Product Designer at PDI ltd.
 							</p>
 						</div>
-						<div className="topics flex  flex-row items-end gap-6 whitespace-nowrap text-sm">
-							<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-								General mentorship
+					</motion.div>
+
+					<motion.div
+						initial={{ x: -100, y: 130 }}
+						animate={{ x: 0, y: 0 }}
+						transition={{
+							type: "tween",
+							duration: 1,
+							ease: "easeInOut",
+						}}
+						className="m-auto mt-44 max-w-screen-xl px-11"
+					>
+						<div className="flex justify-end">
+							{card ? (
+								<button
+									className="bg-primary-500 hover:bg-opacity-70 w-[200px] text-center py-2 text-white rounded-md font-semibold"
+									onClick={showCalendarCard}
+								>
+									Book a session
+								</button>
+							) : (
+								<img
+									src={Backarrow}
+									alt="Back Arrow"
+									className="w-5 h-5 cursor-pointer"
+									onClick={showProfileCard}
+								/>
+							)}
+						</div>
+						<h1 className="mb-5 text-4xl font-semibold">
+							{card ? "Overview" : "Book a Session"}
+						</h1>
+						{card ? (
+							<MentorCard />
+						) : (
+							<MentorScheduleCard mentorEvent={mentorEvents} />
+						)}
+						<motion.div
+							initial={{ scale: 0, opacity: 0 }}
+							animate={{ scale: 1, opacity: 1 }}
+							transition={{ type: "spring", stiffness: 120 }}
+							className="mt-8 inline-flex h-10 w-44 items-center justify-start gap-5"
+						>
+							<div className="h-8 w-8">
+								<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
+									<FaTwitter />
+								</div>
 							</div>
-							<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-								Design career path
+							<div className="h-8 w-8">
+								<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
+									<GrMail />
+								</div>
 							</div>
-							<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-								UX meaning
+							<div className="h-8 w-8">
+								<div className="flex h-8 items-center justify-center rounded-full bg-zinc-300">
+									<FaLinkedinIn />
+								</div>
 							</div>
-							<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
-								Interaction design
+						</motion.div>
+						<div className="mt-11 flex justify-between">
+							<div>
+								<h1 className="whitespace-nowrap text-4xl font-semibold text-black">
+									Community Statistics
+								</h1>
+								<p className="text-xl font-medium text-neutral-700">
+									Top areas of impact
+								</p>
+								<p className="text-base font-normal text-neutral-700">
+									Topics to be discussed during session
+								</p>
+							</div>
+							<div className="topics flex  flex-row items-end gap-6 whitespace-nowrap text-sm">
+								<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+									General mentorship
+								</div>
+								<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+									Design career path
+								</div>
+								<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+									UX meaning
+								</div>
+								<div className="w-30 border-grey flex h-11 items-center justify-center rounded border px-4">
+									Interaction design
+								</div>
 							</div>
 						</div>
+					</motion.div>
+					<div className="mt-20">
+						<Footer />
 					</div>
 				</div>
-				<div className="mt-20">
-					<Footer />
-				</div>
-			</div>}
+			)}
 		</>
 	);
 };
