e23f4aa76ea309b0b2c064842c4516b472b331e5
mruqqie <mruqqie@gmail.com>
2024-04-09 13:02:01 +0100

    modified:   src/components/MentorScheduleCard.jsx 	modified:   src/components/sessionTime.jsx 	modified:   src/components/sessionTime.jsx

--- src/components/MentorScheduleCard.jsx
+++ src/components/MentorScheduleCard.jsx
@@ -1,8 +1,6 @@
-import { Calendar, momentLocalizer } from "react-big-calendar";
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { Modal, Box } from "@mui/material";
-import "react-big-calendar/lib/css/react-big-calendar.css";
 import { ScheduleView } from "../styled/component";
 import SessionTime from "./sessionTime";
 import Alert from "../features/Alert";
@@ -11,8 +9,6 @@ import { useNavigate } from "react-router-dom";
 import { postRequest } from "../utilities/apiClient";
 import { setToken } from "../utilities/axiosClient";
 import Spinner from "./Spinner";
-
-const localizer = momentLocalizer(moment);
 
 function MentorScheduleCard(props) {
 	const { mentorEvent } = props;
@@ -27,47 +23,149 @@ function MentorScheduleCard(props) {
 
 	useEffect(() => {
 		setEvents(mentorEvent);
+		console.log(mentorEvent);
 	}, []);
 
-			return (
-				<div className="relative h-max pb-20">
-            {loading ? (
-                <Spinner loading={loading} />
-            ) : (
-                <div className="flex flex-wrap items-center mx-auto justify-between gap-6 w-max max-w-[90%] border p-4 rounded-2xl shadow-lg">
-                    {mentorEvent?.map((event, index) => (
-                        <React.Fragment key={index}>
-                            {event.available ? (
-                                <div className="rounded-md border shadow-md w-72 h-max">
-                                    <div
-                                        className="text-center text-xl rounded-t-md p-4 font-semibold"
-                                        style={{
-                                            backgroundColor: event.bg,
-                                        }}
-                                    >
-                                        Available
-                                    </div>
-                                    <div className="p-2 flex flex-col items-center">
-                                        <div className="text-3xl font-semibold mb-3">
-                                            {moment(event.start).format("h:mma")}
-                                        </div>
-                                        <div className="text-2xl font-semibold">
-                                            {moment(event.start).format("MMMM D")}
-                                        </div>
-                                        <div className="text-2xl font-semibold">
-                                            {moment(event.start).format("YYYY")}
-                                        </div>
-                                    </div>
-                                </div>
-                            ) : (
-                                <div key={index}></div>
-                            )}
-                        </React.Fragment>
-                    ))}
-                </div>
-            )}
-        </div>
-			);
-		}
+	const handleSelectSlot = ({ start }) => {
+		console.log("start", start);
+		setSelectedDate(start);
+		setOpen(true);
+	};
 
-		export default MentorScheduleCard;
+	const sendAmPm = (date) => {
+		let newDate = new Date(date).getHours();
+		if (newDate < 12) {
+			return `${newDate}AM`;
+		} else {
+			newDate = newDate % 12;
+			return `${newDate}PM`;
+		}
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+		setSelectedDate(null);
+	};
+
+	const handleAddEvent = async () => {
+		try {
+			if (!selectedTime) {
+				return Alert("Please pick a time", "warning");
+			}
+			if (
+				new Date(selectedTime).getHours() <
+				new Date(selectedDate).getHours()
+			) {
+				return Alert(
+					`Please select time from ${sendAmPm(selectedDate)}`,
+					"warning"
+				);
+			}
+			if (!currentEvent) {
+				return Alert("Please pick a schedule", "warning");
+			}
+			setOpen(false);
+			setloading(true);
+			const response = await postRequest(
+				`event/book?time=${new Date(selectedTime)}&event_id=${
+					currentEvent._id
+				}`,
+				{}
+			);
+			if (response && response.success === true) {
+				setOpen(false);
+				setloading(false);
+				return Alert(
+					`Session booked! Please wait for mentor's response`,
+					"success"
+				);
+			} else {
+				setOpen(false);
+				setloading(false);
+				return Alert(`${response.message}`, "warning");
+			}
+			setloading(false);
+		} catch (error) {
+			setOpen(false);
+			setloading(false);
+			return Alert(`${error.message}`, "warning");
+		}
+	};
+
+	const handleSelectEvent = async (event) => {
+		const { title, end, start } = event;
+		setcurrentEvent(event);
+		const token = await getValidToken();
+		if (!token) {
+			return navigation("/auth/signin");
+		}
+		await setToken(token);
+		setSelectedDate(start);
+		return setOpen(true);
+	};
+	return (
+		<div className="relative h-max pb-20">
+			{loading ? (
+				<Spinner loading={loading} />
+			) : (
+				<div className="flex flex-wrap items-center mx-auto justify-evenly gap-6 w-max max-w-[100%] p-4 rounded-2xl">
+					{mentorEvent?.map((event, index) => (
+						<React.Fragment key={index}>
+							{event.available ? (
+								<div
+									className="rounded-md border pb-3 shadow-md w-72 h-max"
+									onClick={() => handleSelectEvent(event)}
+								>
+									<div
+										className="text-center text-xl text-white rounded-t-md p-4 font-semibold"
+										style={{
+											backgroundColor: event.bg,
+										}}
+									>
+										Available
+									</div>
+									<div className="pt-3 flex flex-col items-center">
+										<div className="text-2xl font-semibold mb-3">
+											{moment(event.start).format(
+												"h:mma"
+											)}{" "}
+											-{" "}
+											{moment(event.end).format("h:mma")}
+										</div>
+										<div className="text-xl text-gray-700 font-semibold">
+											{moment(event.start).format(
+												"MMMM D, YYYY"
+											)}
+										</div>
+										<div className="text-2xl font-semibold">
+											{/* {moment(event.start).format("YYYY")} */}
+										</div>
+									</div>
+								</div>
+							) : (
+								<div key={index}></div>
+							)}
+						</React.Fragment>
+					))}
+				</div>
+			)}
+
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="modal-title"
+				aria-describedby="modal-description"
+			>
+				<Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded">
+					<SessionTime
+						handleAddEvent={handleAddEvent}
+						setSelectedTime={setSelectedTime}
+						selectedDate={selectedDate}
+					/>
+				</Box>
+			</Modal>
+		</div>
+	);
+}
+
+export default MentorScheduleCard;
--- src/components/sessionTime.jsx
+++ src/components/sessionTime.jsx
@@ -1,53 +1,39 @@
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { TimePicker } from '@mui/x-date-pickers/TimePicker';
-import { renderTimeViewClock } from '@mui/x-date-pickers/timeViewRenderers';
-import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import { renderTimeViewClock } from "@mui/x-date-pickers/timeViewRenderers";
+import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
 
 const localizer = momentLocalizer(moment);
 
 function SessionTime(props) {
-    const { handleAddEvent, setSelectedTime, selectedDate } = props
-    let minutes = new Date(selectedDate).getMinutes()
-    let hours = new Date(selectedDate).getHours()
-    minutes = minutes > 10 ? minutes : `0${minutes}`
-    hours = hours > 10 ? hours : `0${hours}`
-    const time = hours + ':' + minutes
-    console.log('selectedDate', time)
-    // console.log('selectedDate', new Date(selectedDate).getMinutes())
-    return (
-        <>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                {/* <DemoContainer components={['MobileTimePicker']}> */}
-                <DemoContainer components={['TimePicker']}>
-                    {/* <MobileTimePicker
-                        onChange={(event) => setSelectedTime(new Date(event))}
-                    /> */}
-                    <TimePicker
-                        label="Pick time"
-                        // value={new Date(selectedTime)}
-                        // defaultValue={new Date(selectedTime)}
-                        // minTime={new Date(selectedTime)}
-                        viewRenderers={{
-                            hours: renderTimeViewClock,
-                            minutes: renderTimeViewClock,
-                            seconds: renderTimeViewClock,
-                        }}
-                        onChange={(event) => setSelectedTime(new Date(event))}
-                    />
-                </DemoContainer>
-            </LocalizationProvider>
-            <button
-                onClick={handleAddEvent}
-                className="mt-10 bg-blue-500 text-white px-4 py-2 rounded"
-            >
-                Schedule A Session
-            </button>
-        </>
-    );
+	const { handleAddEvent, setSelectedTime, selectedDate } = props;
+	let minutes = new Date(selectedDate).getMinutes();
+	let hours = new Date(selectedDate).getHours();
+	minutes = minutes > 10 ? minutes : `0${minutes}`;
+	hours = hours > 10 ? hours : `0${hours}`;
+	const time = hours + ":" + minutes;
+	console.log("selectedDate", time);
+	// console.log('selectedDate', new Date(selectedDate).getMinutes())
+	return (
+		<div className=" flex flex-col">
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<TimePicker
+					label="Pick Time"
+					onChange={(event) => setSelectedTime(new Date(event))}
+				/>
+			</LocalizationProvider>
+			<button
+				onClick={handleAddEvent}
+				className="mt-10 bg-blue-500 text-white px-4 py-2 rounded"
+			>
+				Schedule A Session
+			</button>
+		</div>
+	);
 }
 
 export default SessionTime;
