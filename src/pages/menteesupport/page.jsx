3eec3beb09091caba49c0c6ffae3be2d51c36f96
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2024-05-11 10:50:11 +0100

    fixed merged issue

--- src/pages/menteesupport/page.jsx
+++ src/pages/menteesupport/page.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import MenteeSide from "../../components/MenteeSide";
 import Header_Signin from "../../components/Header_Signin";
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-=======
 import { Link } from 'react-router-dom';
->>>>>>> 8fd383a93cc1529c05080038f81abf5c9102e7f4
 
 const MenteeSupport = () => {
   const supportLinks = [
@@ -19,18 +15,6 @@ const MenteeSupport = () => {
       <Header_Signin />
       <div className="flex">
         <MenteeSide />
-<<<<<<< HEAD
-        <section className="w-9/12 mt-[3rem] py-8 px-5">
-          <div className="flex gap-x-3">
-            <div className="border p-6">Help Center</div>
-            <div className="border p-6">Contact us</div>
-            <Link to="/privacy-policy" className="border p-6">
-              Privacy Policy
-            </Link>
-            <div className="border p-6">FAQ</div>
-          </div>
-        </section>
-=======
         <section className="w-9/12 mt-36 py-8 px-5">
                     <h4 className='text-[#454545] text-xl font-semibold mb-12'>Support</h4>
 
@@ -44,7 +28,6 @@ const MenteeSupport = () => {
                         }
                     </div>
                 </section>
->>>>>>> 8fd383a93cc1529c05080038f81abf5c9102e7f4
       </div>
     </div>
   );
