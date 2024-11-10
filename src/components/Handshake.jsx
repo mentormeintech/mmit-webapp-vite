156fabe860b87591b17c68ebee17812afe680f5b
Edrisa Leigh <edrisaleigh@Edrisas-MacBook-Pro.local>
2024-11-10 15:03:55 +0000

    Adjust responsiveness on hero section

--- src/components/Handshake.jsx
+++ src/components/Handshake.jsx
@@ -9,7 +9,9 @@ const Handshake = () => {
         src="images/dim.png"
       />
       <div className="absolute left-0 top-[35%] w-full pl-[9%] 2xl:pl-[12.5%] text-white">
-        <h1 className="mb-[8px] text-5xl font-[600]">Partnership</h1>
+        <h1 className="mb-[8px] text-4xl md:text-5xl font-[600]">
+          Partnership
+        </h1>
         <p className="text-lg font-[500] max-w-xl mx-1 md:mx-0">
           Empowering Innovation Through Collaboration – Together, We Drive the
           Future of Tech
