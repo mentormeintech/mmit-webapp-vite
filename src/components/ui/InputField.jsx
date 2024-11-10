192f2784b6fd3702c722e08abda1cffb11b62c65
Edrisa Leigh <edrisaleigh@Edrisas-MacBook-Pro.local>
2024-11-10 16:43:52 +0000

    Remove dark mode text styles

--- src/components/ui/InputField.jsx
+++ src/components/ui/InputField.jsx
@@ -14,7 +14,7 @@ const InputField = ({
     <div className="mb-3">
       <label
         htmlFor={htmlFor}
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        className="block mb-2 text-sm font-medium text-gray-900"
       >
         {inputLabelText}
         {required && <span className="text-red-500">*</span>}
