72ff0d428bd9f4bae335e59ff91a03649a9ad272
atimscreative <timilehinsunday144@gmail.com>
2024-11-10 17:56:19 +0100

    Schema for Personal Info

--- src/components/personalinfo.jsx
+++ src/components/personalinfo.jsx
@@ -1,49 +1,136 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  fullName: yup.string().required(),
+  gender: yup.string().required(),
+  country: yup.string().required(),
+  phoneNumber: yup.string().required(),
+  about: yup.string().required(),
+});
 
 export default function Personalinfo(props) {
-    const { dashboard } = props
-    return (
-        <form className="w-9/12">
-            <section className="w-6/12">
-                <div className="w-full mb-8">
-                    <div className="flex justify-between items-center mb-3">
-                        <p>Full Name</p>
-                        <label htmlFor="full-name" className="cursor-pointer">Edit</label>
-                    </div>
-                    <input type="text" id="full-name" placeholder="John Doe" className="w-full outline-none" onChange={(event) => console.log(event.target.value)}  value={`${dashboard?.first_name} ${dashboard?.last_name}`} />
-                </div>
+  const { dashboard } = props;
+  const [profile, setProfile] = useState({
+    fullName: `${dashboard?.first_name || ""} ${dashboard?.last_name || ""}`,
+    gender: "",
+    country: dashboard?.country || "",
+    phoneNumber: "",
+    about: dashboard?.about_me || "",
+  });
 
-                <div className="mb-8">
-                    <label className="block mb-3">Gender</label>
-                    <select className="bg-transparent outline-none w-40">
-                        <option>Male</option>
-                        <option>Female</option>
-                    </select>
-                </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-                <div className="mb-8">
-                    <label className="block mb-3">Country</label>
-                    <input type="text" id="" placeholder="NG NIGERIA" onChange={(event) => console.log(event.target.value)} className="outline-none" value={dashboard?.country || 'N/A'}/>
-                </div>
+    setProfile((prev) => {
+      return { ...prev, [name]: value };
+    });
+    console.log(name, value);
+  };
 
-                <div className="w-full mb-8">
-                    <div className="flex justify-between items-center mb-3">
-                        <p>Phone Number</p>
-                        <label htmlFor="phone-number" className="cursor-pointer">Edit</label>
-                    </div>
-                    <input type="text" id="phone-number" placeholder="N/A" className="outline-none border-b-[0.02px] w-full border-[#434343]" />
-                </div>
-            </section>
+  console.log(profile);
 
-            <div className=" mb-8">
-                <div className="flex justify-between items-center mb-3">
-                    <p>About</p>
-                    <label htmlFor="about" className="cursor-pointer">Edit</label>
-                </div>
-                <textarea id="about" className="outline-none h-56 bg-[#FDF8F8] resize-none w-full py-2 px-4" onChange={(event) => console.log(event.target.value)}  defaultValue={dashboard?.about_me || ''}></textarea>
-            </div>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-            <button className="bg-[#FE9B7E] rounded-md w-96 h-11 text-white">save</button>
-        </form>
-    )
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="w-9/12">
+      <section className="w-6/12">
+        <div className="w-full mb-8">
+          <div className="flex justify-between items-center mb-3">
+            <p>Full Name</p>
+            <label htmlFor="full-name" className="cursor-pointer">
+              Edit
+            </label>
+          </div>
+          <input
+            {...register("fullName")}
+            type="text"
+            id="fullName"
+            value={profile?.fullName}
+            placeholder="John Doe"
+            className="w-full outline-none"
+            onChange={(event) => handleChange(event)}
+            // value={`${dashboard?.first_name} ${dashboard?.last_name}`}
+          />
+        </div>
+
+        <div className="mb-8">
+          <label className="block mb-3">Gender</label>
+          <select
+            {...register("gender")}
+            value={profile.gender}
+            onChange={(e) => handleChange(e)}
+            className="bg-transparent outline-none w-40"
+          >
+            <option value="select" selected>
+              select your gender
+            </option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+
+        <div className="mb-8">
+          <label className="block mb-3">Country</label>
+          <input
+            {...register("country")}
+            type="text"
+            id="country"
+            placeholder="NG NIGERIA"
+            onChange={(e) => handleChange(e)}
+            className="outline-none"
+            value={profile?.country || "N/A"}
+          />
+        </div>
+
+        <div className="w-full mb-8">
+          <div className="flex justify-between items-center mb-3">
+            <p>Phone Number</p>
+            <label htmlFor="phone-number" className="cursor-pointer">
+              Edit
+            </label>
+          </div>
+          <input
+            {...register("phoneNumber")}
+            onChange={(e) => handleChange(e)}
+            type="text"
+            id="phone-number"
+            placeholder="N/A"
+            value={profile?.phoneNumber}
+            className="outline-none border-b-[0.02px] w-full border-[#434343]"
+          />
+        </div>
+      </section>
+
+      <div className=" mb-8">
+        <div className="flex justify-between items-center mb-3">
+          <p>About</p>
+          <label htmlFor="about" className="cursor-pointer">
+            Edit
+          </label>
+        </div>
+        <textarea
+          {...register("about")}
+          id="about"
+          className="outline-none h-56 bg-[#FDF8F8] resize-none w-full py-2 px-4"
+          onChange={(e) => handleChange(e)}
+          value={profile?.about || ""}
+        ></textarea>
+      </div>
+
+      <button className="bg-[#FE9B7E] rounded-md w-96 h-11 text-white">
+        save
+      </button>
+    </form>
+  );
 }
--- src/pages/mentorsSettings.jsx
+++ src/pages/mentorsSettings.jsx
@@ -1,80 +1,145 @@
-import React, { useState } from "react"
-import MentorsSettingsComps from "../components/MentorsSettingsComps"
-import MentorSide from "../components/MentorSide"
+import { useState } from "react";
+import MentorsSettingsComps from "../components/MentorsSettingsComps";
+import MentorSide from "../components/MentorSide";
 import { CurrentMentor } from "../components/CurrentMentor";
-import Header_Signin from "../components/Header_Signin"
-import { useSelector } from "react-redux"
+import Header_Signin from "../components/Header_Signin";
+import { useSelector } from "react-redux";
 
 function MentorSettings() {
   const [mentorship, setMentorship] = useState({
     profile: true,
     personalInfo: false,
     login: false,
-    notification: false
-  })
-  const { dashboard } = useSelector(state => state.mentor_me_user)
+    notification: false,
+  });
+  const { dashboard } = useSelector((state) => state.mentor_me_user);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
   function displayProfile() {
     setMentorship({
-      profile: true, personalInfo: false, login: false, notification: false
-    })
+      profile: true,
+      personalInfo: false,
+      login: false,
+      notification: false,
+    });
   }
 
   function displayPersonalInfo() {
     setMentorship({
-      profile: false, personalInfo: true, login: false, notification: false
-    })
+      profile: false,
+      personalInfo: true,
+      login: false,
+      notification: false,
+    });
   }
 
   function displayLogin() {
     setMentorship({
-      profile: false, personalInfo: false, login: true, notification: false
-    })
+      profile: false,
+      personalInfo: false,
+      login: true,
+      notification: false,
+    });
   }
 
   function displayNotification() {
     setMentorship({
-      profile: false, personalInfo: false, login: false, notification: true
-    })
+      profile: false,
+      personalInfo: false,
+      login: false,
+      notification: true,
+    });
   }
 
-  const Mentor = CurrentMentor
+  const Mentor = CurrentMentor;
   return (
     <>
       <Header_Signin />
       <div className="flex text-[#454545]">
-        <MentorSide Mentor={Mentor} setIsMobileMenuOpen={setIsMobileMenuOpen} isMobileMenuOpen={isMobileMenuOpen} />
-        <main className={`flex-1 transition-transform duration-300 ease-in-out ${isMobileMenuOpen ? 'ml-0' : 'ml-64'} p-5`}>
+        <MentorSide
+          Mentor={Mentor}
+          setIsMobileMenuOpen={setIsMobileMenuOpen}
+          isMobileMenuOpen={isMobileMenuOpen}
+        />
+        <main
+          className={`flex-1 transition-transform duration-300 ease-in-out ${
+            isMobileMenuOpen ? "ml-0" : "ml-64"
+          } p-5`}
+        >
           <section className="w-9/12 mt-36 py-8 px-5">
-            <h4 className='text-[#454545] text-xl font-semibold mb-12'>Settings</h4>
+            <h4 className="text-[#454545] text-xl font-semibold mb-12">
+              Settings
+            </h4>
 
             <ul className="flex items-center mb-12 w-10/12 justify-between">
-              <li className={`relative cursor-pointer group ${mentorship.profile ? 'text-[#0F88D9]' : ''}`} onClick={displayProfile}>
-                <div className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] ${mentorship.profile ? 'w-full' : 'w-0'} absolute left-0 -bottom-[3px]`}></div>Mentorship Profile
+              <li
+                className={`relative cursor-pointer group ${
+                  mentorship.profile ? "text-[#0F88D9]" : ""
+                }`}
+                onClick={displayProfile}
+              >
+                <div
+                  className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] ${
+                    mentorship.profile ? "w-full" : "w-0"
+                  } absolute left-0 -bottom-[3px]`}
+                ></div>
+                Mentorship Profile
               </li>
 
-              <li className={`relative cursor-pointer group ${mentorship.personalInfo ? 'text-[#0F88D9]' : ''}`} onClick={displayPersonalInfo}>
-                <div className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] absolute left-0 -bottom-[3px] ${mentorship.personalInfo ? 'w-full' : 'w-0'}`}></div> Personal Info
+              <li
+                className={`relative cursor-pointer group ${
+                  mentorship.personalInfo ? "text-[#0F88D9]" : ""
+                }`}
+                onClick={displayPersonalInfo}
+              >
+                <div
+                  className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] absolute left-0 -bottom-[3px] ${
+                    mentorship.personalInfo ? "w-full" : "w-0"
+                  }`}
+                ></div>{" "}
+                Personal Info
               </li>
 
-              <li className={`relative cursor-pointer group ${mentorship.login ? 'text-[#0F88D9]' : ''}`} onClick={displayLogin}>
-                <div className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] absolute left-0 -bottom-[3px] ${mentorship.login ? 'w-full' : 'w-0'}`}></div>Login and security
+              <li
+                className={`relative cursor-pointer group ${
+                  mentorship.login ? "text-[#0F88D9]" : ""
+                }`}
+                onClick={displayLogin}
+              >
+                <div
+                  className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] absolute left-0 -bottom-[3px] ${
+                    mentorship.login ? "w-full" : "w-0"
+                  }`}
+                ></div>
+                Login and security
               </li>
 
-              <li className={`relative cursor-pointer group ${mentorship.notification ? 'text-[#0F88D9]' : ''}`} onClick={displayNotification}>
-                <div className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] absolute left-0 -bottom-[3px] ${mentorship.notification ? 'w-full' : 'w-0'}`}></div>Message and Notification
+              <li
+                className={`relative cursor-pointer group ${
+                  mentorship.notification ? "text-[#0F88D9]" : ""
+                }`}
+                onClick={displayNotification}
+              >
+                <div
+                  className={`group-hover:w-full transition-all delay-500 ease-in h-[2px] bg-[#0F88D9] absolute left-0 -bottom-[3px] ${
+                    mentorship.notification ? "w-full" : "w-0"
+                  }`}
+                ></div>
+                Message and Notification
               </li>
             </ul>
 
             <div>
-              <MentorsSettingsComps mentorship={mentorship} dashboard={dashboard} />
+              <MentorsSettingsComps
+                mentorship={mentorship}
+                dashboard={dashboard}
+              />
             </div>
           </section>
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default MentorSettings
+export default MentorSettings;
