a92791268f12cc9ac5dd3a161c1ad68116835458
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2024-12-07 16:25:57 +0100

    Update eventCard.jsx

--- src/components/eventCard.jsx
+++ src/components/eventCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { formatEventDescription } from '../utilities/util';
 
 export const EventCard = ({ event }) => {
     // console.log("event.start.timeZone",event)
@@ -9,15 +10,19 @@ export const EventCard = ({ event }) => {
                 <div className="bg-gray-100 shadow-lg rounded-lg p-6">
                     <h2 className="text-2xl font-bold text-gray-700 mb-2">Mentorship Session</h2>
                     <p className="text-gray-500 mb-4">Session with a mentee</p>
-                    <div className="flex flex-col space-y-2">
+                        <div className="flex flex-col space-y-2">
                         <div className="flex items-center justify-between">
+                            <span className="font-semibold text-gray-600">Duration:</span>
+                            <span className="text-gray-700">{formatEventDescription(event?.duration) || '0'}</span>
+                        </div>
+                        {/* <div className="flex items-center justify-between">
                             <span className="font-semibold text-gray-600">Start:</span>
                             <span className="text-gray-700">{new Date(event.start).toLocaleString()}</span>
                         </div>
                         <div className="flex items-center justify-between">
                             <span className="font-semibold text-gray-600">End:</span>
                             <span className="text-gray-700">{new Date(event.end).toLocaleString()}</span>
-                        </div>
+                        </div> */}
                         {/* Optional Time Zone Display */}
                         {/* <div className="flex items-center justify-between mt-4">
                             <span className="font-semibold text-gray-600">Time Zone:</span>
