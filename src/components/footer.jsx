d1a5ce8718832ed61ad59458ae02d74e411acc57
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2024-12-29 20:52:30 +0100

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -62,7 +62,7 @@ export default function Footer() {
         <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-3 gap-y-8 md:gap-8 py-10 max-w-sm mx-auto sm:max-w-3xl lg:max-w-full">
           <div className="col-span-full mb-10 lg:col-span-2 lg:mb-0">
             <a
-              href="https://pagedone.io/"
+              href="/"
               className="flex justify-center lg:justify-start"
             >
               <img
@@ -115,11 +115,11 @@ export default function Footer() {
           <div className="lg:mx-auto text-left ">
             <h4 className="text-lg text-gray-900 font-medium mb-7">Support</h4>
             <ul className="text-sm  transition-all duration-500">
-              <li className="mb-5">
+              {/* <li className="mb-5">
                 <FooterLink href="#" currentRoute={currentRoute}>
                   Join MMIT
                 </FooterLink>
-              </li>
+              </li> */}
               <li className="mb-5">
                 <FooterLink href="#faq" currentRoute={currentRoute}>
                   FAQ
