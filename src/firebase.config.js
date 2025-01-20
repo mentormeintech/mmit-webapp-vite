3b2b6aa97bca318cb47d0c9424bed7b669b12605
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2025-01-20 18:00:42 +0100

    meeting integration

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,25 +1,31 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { GoogleAuthProvider } from "firebase/auth";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDIL8-ezhp2RCkZJhSrUMKX1mN2u-if9hQ",
-//   authDomain: "mmit-event-1.firebaseapp.com",
-//   projectId: "mmit-event-1",
-//   storageBucket: "mmit-event-1.firebasestorage.app",
-//   messagingSenderId: "395687531221",
-//   appId: "1:395687531221:web:d335b2c1ffaea10777c85c",
-//   measurementId: "G-ZFWS627K3V"
-// };
+const firebaseConfigOld = {
+    apiKey: "AIzaSyDIL8-ezhp2RCkZJhSrUMKX1mN2u-if9hQ",
+    authDomain: "mmit-event-1.firebaseapp.com",
+    projectId: "mmit-event-1",
+    storageBucket: "mmit-event-1.firebasestorage.app",
+    messagingSenderId: "395687531221",
+    appId: "1:395687531221:web:d335b2c1ffaea10777c85c",
+    measurementId: "G-ZFWS627K3V"
+};
+const firebaseConfig = {
+    apiKey: "AIzaSyDIL8-ezhp2RCkZJhSrUMKX1mN2u-if9hQ",
+    authDomain: "mmit-event-1.firebaseapp.com",
+    projectId: "mmit-event-1",
+    storageBucket: "mmit-event-1.firebasestorage.app",
+    messagingSenderId: "395687531221",
+    appId: "1:395687531221:web:da20c54c05e1a53977c85c",
+    measurementId: "G-V4JG4WBP3Q"
+  };
 
-// // Initialize Firebase
-// const firebaseApp = initializeApp(firebaseConfig);
-// const firebaseAnalytics = getAnalytics(app);
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
+export const firebaseAnalytics = getAnalytics(firebaseApp);
 
 
-// const provider = new GoogleAuthProvider();
+export const googleProvider = new GoogleAuthProvider();
