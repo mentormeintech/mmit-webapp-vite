f5bb12246e62e2b9f5577f48a1921174290a4c53
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2025-01-23 20:28:28 +0100

    Update page.jsx

--- src/pages/about-us/page.jsx
+++ src/pages/about-us/page.jsx
@@ -7,17 +7,16 @@ import AboutLeft from '../../components/AboutLeft';
 
 
 const AboutUs = () => {
-  return (
-    <div>
-      <Header />
-      <Whoweare />
-          <div className="pt-10 max-w-6xl lg:mx-auto mx-4">
-              
-        <AboutLeft />
-      </div>
-      <Footer />
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <Whoweare />
+            <div className="pt-10 max-w-6xl lg:mx-auto mx-4">
+                <AboutLeft />
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default AboutUs
