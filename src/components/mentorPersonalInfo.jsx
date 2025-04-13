d7b34dcbda895d191295701f59ba3d0f18354573
mruqqie <mruqqie@gmail.com>
2025-04-13 19:47:08 +0100

    modified:   src/components/menteeChangePassword.jsx 	modified:   src/components/mentorChangePassword.jsx 	modified:   src/components/mentorPersonalInfo.jsx 	modified:   src/components/mentorProfileInfo.jsx 	modified:   src/components/personalinfo.jsx

--- src/components/mentorPersonalInfo.jsx
+++ src/components/mentorPersonalInfo.jsx
@@ -1,102 +1,134 @@
-import { useState, useRef } from 'react'
-import DefaultInput from './defaultInput'
-import Alert from '../features/Alert';
+import { useState, useRef } from "react";
+import DefaultInput from "./defaultInput";
+import Alert from "../features/Alert";
 
 export default function MentorPersonalinfo(props) {
-    const { dashboard, onSubmit } = props
+	const { dashboard, onSubmit } = props;
 
-    const [formData, setFormData] = useState({
-        first_name: `${dashboard?.first_name}`,
-        last_name: `${dashboard?.last_name}`,
-        country: dashboard?.country || '',
-        gender: dashboard?.gender || '',
-        mobile: dashboard?.mobile || '',
-        about_me: dashboard?.about_me || '',
-    });
+	const [formData, setFormData] = useState({
+		first_name: `${dashboard?.first_name}`,
+		last_name: `${dashboard?.last_name}`,
+		country: dashboard?.country || "",
+		gender: dashboard?.gender || "",
+		mobile: dashboard?.mobile || "",
+		about_me: dashboard?.about_me || "",
+	});
 
-    const handleChange = (event) => {
-        try {
-            const { name, value } = event.target;
-            setFormData({ ...formData, [name]: value });
-        } catch (error) {
-            Alert(error.message, 'error')
-        }
-    };
+	const handleChange = (event) => {
+		try {
+			const { name, value } = event.target;
+			setFormData({ ...formData, [name]: value });
+		} catch (error) {
+			Alert(error.message, "error");
+		}
+	};
 
-    async function submitForm(event) {
-        event.preventDefault()
-        onSubmit(formData);
-    }
-    const inputRef = useRef(null);
-    return (
-        <form className="w-9/12">
-            <section className="w-6/12">
-                <div className="w-full mb-8">
-                    <div className="flex justify-between items-center mb-3">
-                        <p>First Name</p>
-                        {/* <label htmlFor="full-name" className="cursor-pointer">Edit</label> */}
-                    </div>
-                    <DefaultInput ref={inputRef} type="text" id="first_name" placeholder="First name"
-                        value={formData.first_name}
-                        onChange={handleChange}
-                        editable="false" />
-                    {/* <UserInlineInput type="text" id="full-name" placeholder="John Doe" className="w-full outline-none" value={`${dashboard?.first_name} ${dashboard?.last_name}`} 
+	async function submitForm(event) {
+		event.preventDefault();
+		onSubmit(formData);
+	}
+	const inputRef = useRef(null);
+	return (
+		<form className="">
+			<section className="md:w-[50%]">
+				<div className="w-full mb-8">
+					<div className="flex justify-between items-center mb-3">
+						<p>First Name</p>
+						{/* <label htmlFor="full-name" className="cursor-pointer">Edit</label> */}
+					</div>
+					<DefaultInput
+						ref={inputRef}
+						type="text"
+						id="first_name"
+						placeholder="First name"
+						value={formData.first_name}
+						onChange={handleChange}
+						editable="false"
+					/>
+					{/* <UserInlineInput type="text" id="full-name" placeholder="John Doe" className="w-full outline-none" value={`${dashboard?.first_name} ${dashboard?.last_name}`} 
                         editable="false"/> */}
-                    {/* <input type="text" id="full-name" placeholder="John Doe" className="w-full outline-none" onChange={(event) => console.log(event.target.value)}  value={`${dashboard?.first_name} ${dashboard?.last_name}`} /> */}
-                </div>
-                <div className="w-full mb-8">
-                    <div className="flex justify-between items-center mb-3">
-                        <p>Last Name</p>
-                    </div>
-                    <DefaultInput ref={inputRef} type="text" id="last_name" placeholder="Last name"
-                        value={formData.last_name}
-                        onChange={handleChange}
-                        editable="false" />
-                </div>
+					{/* <input type="text" id="full-name" placeholder="John Doe" className="w-full outline-none" onChange={(event) => console.log(event.target.value)}  value={`${dashboard?.first_name} ${dashboard?.last_name}`} /> */}
+				</div>
+				<div className="w-full mb-8">
+					<div className="flex justify-between items-center mb-3">
+						<p>Last Name</p>
+					</div>
+					<DefaultInput
+						ref={inputRef}
+						type="text"
+						id="last_name"
+						placeholder="Last name"
+						value={formData.last_name}
+						onChange={handleChange}
+						editable="false"
+					/>
+				</div>
+				<div className="mb-8">
+					<label className="block mb-3">Gender</label>
+					<select
+						className="bg-transparent outline-none w-full border-b-[0.02px] border-b-[#434343] box-shadow-none px-2 mb-5"
+						name="gender"
+						value={formData.gender}
+						onChange={handleChange}
+					>
+						<option value="female">Female</option>
+						<option value="male">Male</option>
+					</select>
+				</div>
+				<div className="mb-8">
+					<label className="block mb-3">Country</label>
+					<DefaultInput
+						ref={inputRef}
+						type="text"
+						editable="false"
+						id=""
+						placeholder="Country"
+						value={formData.country}
+						name="country"
+						onChange={handleChange}
+					/>
+				</div>
+				<div className="w-full mb-8">
+					<div className="flex justify-between items-center mb-3">
+						<p>Phone Number</p>
+						<label htmlFor="mobile" className="cursor-pointer">
+							Edit
+						</label>
+					</div>
+					<DefaultInput
+						ref={inputRef}
+						type="text"
+						id="mobile"
+						placeholder="Phone Number"
+						value={formData.mobile}
+						name="mobile"
+						onChange={handleChange}
+					/>
+				</div>
+			</section>
 
-                <div className="mb-8">
-                    <label className="block mb-3">Gender</label>
-                    <select
-                        className="bg-transparent outline-none w-full border-b-[0.02px] border-b-[#434343] box-shadow-none px-2 mb-5"
-                        name="gender"
-                        value={formData.gender}
-                        onChange={handleChange}
-                    >
-                        <option value="female">Female</option>
-                        <option value="male">Male</option>
-                    </select>
-                </div>
+			<div className="md:w-[50%] mb-8">
+				<div className="flex justify-between items-center mb-3">
+					<p>About</p>
+					<label htmlFor="about_me" className="cursor-pointer">
+						Edit
+					</label>
+				</div>
+				<textarea
+					id="about_me"
+					className="outline-none h-56 bg-transparent resize-none w-full py-2 px-4"
+					value={formData.about_me}
+					name="about_me"
+					onChange={handleChange}
+				></textarea>
+			</div>
 
-                <div className="mb-8">
-                    <label className="block mb-3">Country</label>
-                    <DefaultInput ref={inputRef} type="text" editable="false" id="" placeholder="Country" value={formData.country}
-                        name="country"
-                        onChange={handleChange} />
-                </div>
-
-                <div className="w-full mb-8">
-                    <div className="flex justify-between items-center mb-3">
-                        <p>Phone Number</p>
-                        <label htmlFor="mobile" className="cursor-pointer">Edit</label>
-                    </div>
-                    <DefaultInput ref={inputRef} type="text" id="mobile" placeholder="Phone Number"
-                        value={formData.mobile}
-                        name="mobile"
-                        onChange={handleChange} />
-                </div>
-            </section>
-
-            <div className=" mb-8">
-                <div className="flex justify-between items-center mb-3">
-                    <p>About</p>
-                    <label htmlFor="about_me" className="cursor-pointer">Edit</label>
-                </div>
-                <textarea id="about_me" className="outline-none h-56 bg-transparent resize-none w-full py-2 px-4" value={formData.about_me}
-                    name="about_me"
-                    onChange={handleChange}></textarea>
-            </div>
-
-            <button className="bg-[#FE9B7E] rounded-md w-96 h-11 text-white" onClick={submitForm}>save</button>
-        </form>
-    )
+			<button
+				className="bg-[#FE9B7E] rounded-md w-[100%] md:w-[50%] h-11 text-white"
+				onClick={submitForm}
+			>
+				Save
+			</button>
+		</form>
+	);
 }
--- src/components/personalinfo.jsx
+++ src/components/personalinfo.jsx
@@ -1,133 +1,140 @@
-import { useState } from 'react'
-import Alert from '../features/Alert';
+import { useState } from "react";
+import Alert from "../features/Alert";
 
 export default function Personalinfo(props) {
-  const { dashboard, onSubmit } = props
+	const { dashboard, onSubmit } = props;
 
+	const [formData, setFormData] = useState({
+		first_name: `${dashboard?.first_name}`,
+		last_name: `${dashboard?.last_name}`,
+		country: dashboard?.country || "",
+		gender: dashboard?.gender || "",
+		mobile: dashboard?.mobile || "",
+		about_me: dashboard?.about_me || "",
+	});
 
-  const [formData, setFormData] = useState({
-    first_name: `${dashboard?.first_name}`,
-    last_name: `${dashboard?.last_name}`,
-    country: dashboard?.country || '',
-    gender: dashboard?.gender || '',
-    mobile: dashboard?.mobile || '',
-    about_me: dashboard?.about_me || '',
-  });
+	const handleChange = (event) => {
+		try {
+			const { name, value } = event.target;
+			setFormData({ ...formData, [name]: value });
+			console.log("value", value);
+		} catch (error) {
+			Alert(error.message, "error");
+		}
+	};
 
+	async function submitForm(event) {
+		event.preventDefault();
+		onSubmit(formData);
+	}
 
-  const handleChange = (event) => {
-    try {
-      const { name, value } = event.target;
-      setFormData({ ...formData, [name]: value });
-      console.log("value", value)
-    } catch (error) {
-      Alert(error.message, 'error')
-    }
-  };
+	return (
+		<form className="w-full">
+			{/* Personal Information Section */}
+			<section className="md:w-[70%]">
+				{/* Full Name Field */}
+				<div className="w-[100%] lg:w-[80%] mb-8">
+					<div className="flex justify-between items-center mb-3">
+						<p className="text-sm lg:text-base">Full Name</p>
+					</div>
+					<input
+						type="text"
+						id="full-name"
+						placeholder="John Doe"
+						className="bg-transparent w-full outline-none border-b-[0.02px] border-[#434343] pb-2 text-sm lg:text-base"
+						editable="false"
+						name="first_name"
+						onChange={handleChange}
+						value={`${formData?.first_name}`}
+					/>
+				</div>
+				<div className="w-[100%] lg:w-[80%] mb-8">
+					<div className="flex justify-between items-center mb-3">
+						<p className="text-sm lg:text-base">Full Name</p>
+					</div>
+					<input
+						type="text"
+						id="full-name"
+						placeholder="John Doe"
+						className="bg-transparent w-full outline-none border-b-[0.02px] border-[#434343] pb-2 text-sm lg:text-base"
+						name="last_name"
+						editable="false"
+						onChange={handleChange}
+						value={`${formData?.last_name}`}
+					/>
+				</div>
+				{/* Gender Field */}
+				<div className="w-[100%] lg:w-[80%] mb-8">
+					<label className="block mb-3 text-sm lg:text-base">
+						Gender
+					</label>
+					<select
+						className="bg-transparent outline-none w-full border-b-[0.02px] border-b-[#434343] box-shadow-none px-2 mb-5"
+						name="gender"
+						value={formData.gender}
+						onChange={handleChange}
+					>
+						<option value="female">Female</option>
+						<option value="male">Male</option>
+					</select>
+				</div>
 
-  async function submitForm(event) {
-    event.preventDefault()
-    onSubmit(formData);
-  }
+				{/* Country Field */}
+				<div className="w-[100%] lg:w-[80%] mb-8">
+					<label className="block mb-3 text-sm lg:text-base">
+						Country
+					</label>
+					<input
+						type="text"
+						id="country"
+						placeholder="NG NIGERIA"
+						className="outline-none border-b-[0.02px] border-[#434343] pb-2 w-full text-sm lg:text-base"
+						name="country"
+						onChange={handleChange}
+						value={formData?.country}
+					/>
+				</div>
 
-  return (
-    <form className="w-full lg:w-9/12">
-      {/* Personal Information Section */}
-      <section className="w-full lg:w-6/12">
-        {/* Full Name Field */}
-        <div className="w-full mb-8">
-          <div className="flex justify-between items-center mb-3">
-            <p className="text-sm lg:text-base">Full Name</p>
-          </div>
-          <input
-            type="text"
-            id="full-name"
-            placeholder="John Doe"
-            className="bg-transparent w-full outline-none border-b-[0.02px] border-[#434343] pb-2 text-sm lg:text-base"
-            editable="false" 
-            name="first_name"
-            onChange={handleChange}
-            value={`${formData?.first_name}`}
-          />
-        </div>
-        <div className="w-full mb-8">
-          <div className="flex justify-between items-center mb-3">
-            <p className="text-sm lg:text-base">Full Name</p>
-          </div>
-          <input
-            type="text"
-            id="full-name"
-            placeholder="John Doe"
-            className="bg-transparent w-full outline-none border-b-[0.02px] border-[#434343] pb-2 text-sm lg:text-base"
-            name="last_name"
-            editable="false" 
-            onChange={handleChange}
-            value={`${formData?.last_name}`}
-          />
-        </div>
-        {/* Gender Field */}
-        <div className="w-full mb-8">
-          <label className="block mb-3 text-sm lg:text-base">Gender</label>
-          <select
-            className="bg-transparent outline-none w-full border-b-[0.02px] border-b-[#434343] box-shadow-none px-2 mb-5"
-            name="gender"
-            value={formData.gender}
-            onChange={handleChange}
-          >
-            <option value="female">Female</option>
-            <option value="male">Male</option>
-          </select>
-        </div>
+				{/* Phone Number Field */}
+				<div className="w-[100%] lg:w-[80%] mb-8">
+					<div className="flex justify-between items-center mb-3">
+						<p className="text-sm lg:text-base">Phone Number</p>
+					</div>
+					<input
+						type="text"
+						id="phone-number"
+						placeholder="N/A"
+						className="outline-none border-b-[0.02px] w-full border-[#434343] pb-2 text-sm lg:text-base"
+						name="mobile"
+						onChange={handleChange}
+						value={formData.mobile}
+					/>
+				</div>
+			</section>
 
-        {/* Country Field */}
-        <div className="mb-8">
-          <label className="block mb-3 text-sm lg:text-base">Country</label>
-          <input
-            type="text"
-            id="country"
-            placeholder="NG NIGERIA"
-            className="outline-none border-b-[0.02px] border-[#434343] pb-2 w-full text-sm lg:text-base"
-            name="country"
-            onChange={handleChange}
-            value={formData?.country}
-          />
-        </div>
+			{/* About Section */}
+			<div className="md:w-[70%] mb-8">
+				<div className="flex justify-between items-center mb-3">
+					<p className="text-sm lg:text-base">About</p>
+				</div>
+				<textarea
+					id="about"
+					className="bg-transparent outline-none h-40 lg:h-56 bg-[#FDF8F8] resize-none w-[100%] lg:w-[80%] py-2 px-4 text-sm lg:text-base border border-black border-[.5px]"
+					name="about_me"
+					onChange={handleChange}
+					value={formData.about_me}
+				></textarea>
+			</div>
 
-        {/* Phone Number Field */}
-        <div className="w-full mb-8">
-          <div className="flex justify-between items-center mb-3">
-            <p className="text-sm lg:text-base">Phone Number</p>
-          </div>
-          <input
-            type="text"
-            id="phone-number"
-            placeholder="N/A"
-            className="outline-none border-b-[0.02px] w-full border-[#434343] pb-2 text-sm lg:text-base"
-            name="mobile"
-            onChange={handleChange}
-            value={formData.mobile}
-          />
-        </div>
-      </section>
-
-      {/* About Section */}
-      <div className="mb-8">
-        <div className="flex justify-between items-center mb-3">
-          <p className="text-sm lg:text-base">About</p>
-        </div>
-        <textarea
-          id="about"
-          className="bg-transparent outline-none h-40 lg:h-56 bg-[#FDF8F8] resize-none w-full py-2 px-4 text-sm lg:text-base border border-black border-[.5px]"
-          name="about_me"
-          onChange={handleChange}
-          value={formData.about_me}
-        ></textarea>
-      </div>
-
-      {/* Save Button */}
-      <button className="bg-[#FE9B7E] rounded-md w-full lg:w-96 h-11 text-white text-sm lg:text-base hover:bg-[#FF8A6A] transition-colors duration-300" onClick={submitForm}>
-        Save
-      </button>
-    </form>
-  )
+			{/* Save Button */}
+			<div className="md:w-[70%]">
+				<button
+					className="bg-[#FE9B7E] rounded-md w-[100%] lg:w-[80%] h-11 text-white text-sm lg:text-base hover:bg-[#FF8A6A] transition-colors duration-300"
+					onClick={submitForm}
+				>
+					Save
+				</button>
+			</div>
+		</form>
+	);
 }
